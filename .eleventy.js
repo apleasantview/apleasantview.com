78fa093826e5976a1bb0547195e4a229cfa02e23
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-03-09 22:40:35 +0100

    Base 11ty config

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+export default function (eleventyConfig) {
+	return {
+		dir: {
+			input: "src",
+			output: "dist",
+			data: "_data",
+			includes: "_includes"
+		},
+		htmlTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
+		templateFormats: ["html", "njk", "md"]
+	}
+};
