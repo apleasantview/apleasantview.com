736345ca967f49c6d056f53bb220b5ffc0ab6baa
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-03-11 00:34:36 +0100

    Tabs

--- _11ty/plugins/pdehaan-debug/.eleventy.js
+++ _11ty/plugins/pdehaan-debug/.eleventy.js
@@ -2,12 +2,12 @@ import { inspect } from 'util';
 
 /* https://github.com/pdehaan/eleventy-plugin-debug */
 export default function (eleventyConfig, options = {}) {
-  options = {
-    space: 0,
-    ...options
-  };
+	options = {
+		space: 0,
+		...options
+	};
 
-  eleventyConfig.addFilter('inspect', inspect);
-  eleventyConfig.addFilter('json', (obj, space = options.space) => JSON.stringify(obj, null, space));
-  eleventyConfig.addFilter('keys', obj => Object.keys(obj).sort());
+	eleventyConfig.addFilter('inspect', inspect);
+	eleventyConfig.addFilter('json', (obj, space = options.space) => JSON.stringify(obj, null, space));
+	eleventyConfig.addFilter('keys', obj => Object.keys(obj).sort());
 };
