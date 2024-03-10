a04798343b4a7077e68d5d84e7acf2b13460b32f
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-03-10 22:07:30 +0100

    Move CSS template format to plugins territory
    I'm probably going to end up with what
     5t3ph/eleventy-plugin-lightning-css does.

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,12 @@
 import debug from "./_11ty/plugins/pdehaan-debug/.eleventy.js";
+import assets from "./_11ty/plugins/assets-citizens/.eleventy.js";
 
 /** @param { import("@11ty/eleventy/src/UserConfig.js").default } eleventyConfig */
 export default function (eleventyConfig) {
-	eleventyConfig.addTemplateFormats("css");
-	eleventyConfig.addExtension("css", {
-		outputFileExtension: "css",
-		compile: async function (_inputContent, inputPath) {
-			let output = _inputContent;
-
-			return async () => {
-				return output;
-			};
-		}
-	});
 
 	/* Plugins */
 	eleventyConfig.addPlugin(debug);
+	eleventyConfig.addPlugin(assets);
 
 	return {
 		dir: {
---
+++ _11ty/plugins/assets-citizens/.eleventy.js
@@ -0,0 +1,14 @@
+export default function (eleventyConfig) {
+	eleventyConfig.addTemplateFormats("css");
+
+	eleventyConfig.addExtension("css", {
+		outputFileExtension: "css",
+		compile: async function (_inputContent, inputPath) {
+			let output = _inputContent;
+
+			return async () => {
+				return output;
+			};
+		}
+	});
+}
