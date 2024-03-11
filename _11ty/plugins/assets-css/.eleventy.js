b7caea1c38947c990a8176bb2060bc13b40717bb
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-03-11 22:55:11 +0100

    Switch to lightningcss

--- _11ty/plugins/assets-css/.eleventy.js
+++ _11ty/plugins/assets-css/.eleventy.js
@@ -1,13 +1,29 @@
+import path from "node:path"
+import browserslist from "browserslist";
+import { bundle, browserslistToTargets } from "lightningcss";
+
 export default function (eleventyConfig) {
 	eleventyConfig.addTemplateFormats("css");
 
 	eleventyConfig.addExtension("css", {
 		outputFileExtension: "css",
 		compile: async function (_inputContent, inputPath) {
-			let output = _inputContent;
+			let input = path.basename(inputPath);
+
+			let targets = browserslistToTargets(browserslist("> 0.5%, last 2 versions"));
+
+			if (input != "index.css") {
+				return;
+			}
 
 			return async () => {
-				return output;
+				let { code } = bundle({
+					filename: inputPath,
+					minify: true,
+					targets
+				})
+				console.log(input);
+				return code;
 			};
 		}
 	});
