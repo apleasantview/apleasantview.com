a3b886c6b231b686a03d4a9c8d389f527fe1f58c
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-04-28 01:15:39 +0200

    Add a translations collection

--- _11ty/plugins/i18n-feature/.eleventy.js
+++ _11ty/plugins/i18n-feature/.eleventy.js
@@ -16,29 +16,31 @@ export default function (eleventyConfig, options = {}) {
 
 	eleventyConfig.addGlobalData("i18n", config);
 
-	eleventyConfig.addCollection("languages", function(collection) {
-		const site = eleventyConfig.globalData.site;
+	eleventyConfig.addCollection("translations", function (collection) {
+		const allPages = collection.getAll();
+		const translations = {};
 
-		const localeCodes = Object.keys(site.i18n.languages);
-		const byLocale = {};
-	
-		if (!collection) {
-			console.error("Collection API is not available.");
-			return [];
-		}
-	
-		localeCodes.forEach(item => {
-			byLocale[item] = [];
-		})
-	
-		collection.getAll().forEach(item => {
-			if (item.data.locale && localeCodes.includes(item.data.locale)) {
-				byLocale[item.data.locale].push(item);
+		allPages.forEach(page => {
+			if (page.data.translationKey) {
+				const translationKey = page.data.translationKey;
+				const language = page.data.locale;
+				const url = page.url;
+				const title = page.data.title;
+
+				if (!translations[translationKey]) {
+					translations[translationKey] = [];
+				}
+				translations[translationKey].push({
+					title: title,
+					language: language,
+					url: url
+				});
 			}
 		});
 
-		return byLocale;
+		return translations;
 	});
+
 	let extensionMap;
 	eleventyConfig.on("eleventy.extensionmap", (map) => {
 		extensionMap = map;
