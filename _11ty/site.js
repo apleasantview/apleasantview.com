e39b437f05cd2f69cfdf8eddaca12ad0b7b9c9c3
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-05-01 17:03:49 +0200

    Clean up basic site data

--- _11ty/site.js
+++ _11ty/site.js
@@ -1,10 +1,9 @@
 import { i18n } from "./i18n.js";
-import { sitemap } from "./sitemap.js";
 
 export const site = {
-	baseURL: "https://www.apleasantview.com",
+	baseURL: "https://www.apleasantview.com/",
 	title: "a pleasant view",
 	description: "Website subscriptions for small businesses and agencies.",
-	...i18n,
-	sitemap
+	defaultLanguage: "en",
+	...i18n
 }
