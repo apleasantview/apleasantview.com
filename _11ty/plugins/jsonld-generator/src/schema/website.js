3b9746a41040a4c32b93efc7cd4c442f4edf57d7
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-05-04 19:53:52 +0200

    Add webpage schema and website schema mod

--- _11ty/plugins/jsonld-generator/src/schema/website.js
+++ _11ty/plugins/jsonld-generator/src/schema/website.js
@@ -2,7 +2,8 @@ const baseSchema = {
 	"@type": "WebSite",
 	"@id": "https://www.example.com/#website",
 	url: "https://www.example.com/",
-	name: "Website name"
+	name: "Website name",
+	publisher: "https://www.example.com/#organization"
 }
 
 export function orgSchema(userValues = {}) {
