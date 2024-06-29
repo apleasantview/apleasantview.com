2388468ded1ca593459c7a7c8e13cb2f1ae4a9a8
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-06-29 23:03:31 +0200

    Commit some WIP

---
+++ _11ty/jsonLD.js
@@ -0,0 +1,13 @@
+export const jsonLD = {
+	organization: {
+		url: "https://www.apleasantview.com/",
+		name: "a pleasant view",
+		extend: {
+
+		}
+	},
+	website: {
+		url: "https://apleasantview.com/",
+		name: "a pleasant view"
+	}
+}
