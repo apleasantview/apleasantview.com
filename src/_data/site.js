4bd9b852c2b06ef8e6e19c7434987736a9d05f83
Cristovao Verstraeten <cristovao@apleasantview.com>
2024-07-08 14:08:50 +0200

    Remove i18n import

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,6 @@
-import i18n from "./i18n.js";
-
 export default {
 	baseURL: "https://www.apleasantview.com/",
 	title: "a pleasant view",
 	description: "Website subscriptions for small businesses and agencies.",
-	defaultLanguage: "en",
-	...i18n
+	defaultLanguage: "en"
 }
