8764d70dbb117df1325298040a9bd3848af64f85
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-04-12 00:31:37 +0200

    11tydata fix

--- _11ty/plugins/assets-esbuild.js
+++ _11ty/plugins/assets-esbuild.js
@@ -3,12 +3,15 @@ import * as esbuild from "esbuild";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default function assetsESBuild(eleventyConfig) {
+	// Only add js as a template format for files in assets/js
 	eleventyConfig.addTemplateFormats("js");
 
 	eleventyConfig.addExtension("js", {
 		outputFileExtension: "js",
 		compile: async function (_inputContent, inputPath) {
-			if (!inputPath.startsWith("./src/assets/js/") || path.basename(inputPath) !== "index.js") {
+			// Only process files in assets/js directory
+			if (!inputPath.startsWith("./src/assets/js/") ||
+				path.basename(inputPath) !== "index.js") {
 				return;
 			}
 
@@ -24,5 +27,17 @@ export default function assetsESBuild(eleventyConfig) {
 				return result.outputFiles[0].text;
 			}
 		}
+	});
+
+	// Override the default collection behavior. Adding js as template format and extension collects 11tydata.js files.
+	eleventyConfig.addCollection("all", function (collectionApi) {
+		return collectionApi.getAll().filter(item => {
+			// Skip 11tydata.js files
+			if (item.inputPath.endsWith('11tydata.js')) {
+				return false;
+			}
+
+			return true;
+		});
 	});
 };
