71c78304b30bc6fcbe5eace3f2c2443534c89f31
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-24 01:12:20 +0100

    Normalize path

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -2,21 +2,21 @@ export default {
 	defaultLanguage: "en",
 	languages: {
 		en: {
-			contentDir: "content/en",
+			contentDir: "content/en/",
 			languageCode: "en",
 			languageName: "English",
 			title: "a pleasant view",
 			description: "Digital Services for Amsterdam West & the Jordaan"
 		},
 		nl: {
-			contentDir: "content/nl",
+			contentDir: "content/nl/",
 			languageCode: "nl",
 			languageName: "Nederlands",
 			title: "a pleasant view",
 			description: "Webdesign en ontwikkeling abonnementen."
 		},
 		fr: {
-			contentDir: "content/fr",
+			contentDir: "content/fr/",
 			languageCode: "fr",
 			languageName: "Français",
 			title: "a pleasant view",
