af1611418ca5c8d23f63dec52fdf59651df9eee3
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-24 02:05:37 +0100

    Add image preload in `head.js`

--- src/_data/head.js
+++ src/_data/head.js
@@ -1,4 +1,4 @@
 export default {
-  link: [{ rel: "stylesheet", href: "/assets/css/index.css" }],
+  link: [{ rel: "stylesheet", href: "/assets/css/index.css" }, { rel: "preload", href: "/assets/img/2-_FQp-yOr-1440.avif", as: "image", fetchpriority: "high"}],
   script: [{ src: "/assets/js/index.js", defer: true }]
 };
