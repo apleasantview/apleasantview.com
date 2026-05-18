55f48a8e3adfe43d9684929ea0b056d47b499e1e
Cristovao Verstraeten <cristovao@apleasantview.com>
2026-05-18 17:12:03 +0200

    Update project configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-
+// eslint.config.mjs
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+	globalIgnores(['dist/**', 'temp/**']),
+	{
+		files: ['**/*.{js,mjs,cjs}'],
+		...js.configs.recommended
+	},
+	{
+		files: ['**/*.{js,mjs,cjs}', '**/*.11tydata.js', 'src/_data/**/*.{js,mjs,cjs}'],
+		languageOptions: { globals: { ...globals.node } }
+	},
+	{
+		files: ['src/assets/**/*.{js,mjs,cjs}'],
+		languageOptions: { globals: { ...globals.browser } }
+	}
 ]);
